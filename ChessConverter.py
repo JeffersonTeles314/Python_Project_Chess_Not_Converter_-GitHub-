07c1fd1867a1d3728623b5dee96045276705d67d
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2023-05-01 22:31:27 -0500

    hjghj

--- ChessConverter.py
+++ ChessConverter.py
@@ -1,15 +1,19 @@
-var_vig = str(input("Pgn: ")) 
-#Rei 
-if 'R' in var_vig: 
-    var_vig = var_vig.replace('R', 'K') 
-#Rainha 
-if 'D' in var_vig: 
-    var_vig = var_vig.replace('D', 'Q') 
-#Torre 
-if 'T' in var_vig: 
-    var_vig = var_vig.replace('T', 'R') 
-#Bispo 
-#Cavalo 
-if 'C' in var_vig: 
-    var_vig = var_vig.replace('C', 'N') 
-print(var_vig)
+
+
+def convertstr(var_vig):
+    var_vig = str(input("Pgn: ")) 
+    # Rei 
+    if 'R' in var_vig: 
+        var_vig = var_vig.replace('R', 'K') 
+    # Rainha 
+    if 'D' in var_vig: 
+        var_vig = var_vig.replace('D', 'Q') 
+    # Torre 
+    if 'T' in var_vig: 
+        var_vig = var_vig.replace('T', 'R') 
+    # Bispo
+        # Não Necessário
+    # Cavalo 
+    if 'C' in var_vig: 
+        var_vig = var_vig.replace('C', 'N') 
+    return var_vig
